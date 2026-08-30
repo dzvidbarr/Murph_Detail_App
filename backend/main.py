aa42d4aceb652f474a4fa2a9447fed7fe8c7a637
David <dbarr300@hotmail.com>
2026-08-30 06:37:40 -0400

    Allow Vercel domain

--- backend/main.py
+++ backend/main.py
@@ -24,8 +24,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
-    allow_origin_regex = r"https://murph-detail-[a-z0-9]+-healing-orange\.vercel\.app",
+    allow_origins = ["http://localhost:5173", "https://murph-detail-app.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
